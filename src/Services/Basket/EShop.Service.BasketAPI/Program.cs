e1386dc9abfbdc20beb3f65be806e8de54ed8b50
hungnqph19112 <nqh1703k2@gmail.com>
2025-03-22 15:01:21 +0700

    Basket Service API: Publish BasketCheckout Event

--- src/Services/Basket/EShop.Service.BasketAPI/Program.cs
+++ src/Services/Basket/EShop.Service.BasketAPI/Program.cs
@@ -11,9 +11,11 @@ global using BuidingBlocks.Exceptions;
 global using BuidingBlocks.Exceptions.Handler;
 global using EShop.Service.BasketAPI.Domain.Entities;
 global using EShop.Service.BasketAPI.Persistence.Repositories;
+global using EShop.Service.BasketAPI.DTOs;
 using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using EShop.Service.DiscountGRPC;
+using BuildingBlocks.Messaging.MassTransit;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,26 +23,31 @@ builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnC
                      .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true);
 
 // Add services to the container.
-builder.Services.AddCarter(); // Registering API endpoint - Carter
 
+// Registering Application Services
+builder.Services.AddCarter(); // Registering API endpoint - Carter
 builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssembly(typeof(Program).Assembly);
     config.AddOpenBehavior(typeof(ValidationBehavior<,>)); //CQRS Pineline behavior: Validation Middleware - MediatR
     config.AddOpenBehavior(typeof(LoggingBehavior<,>)); //CQRS Pineline behavior: Logging Middleware - MediatR
 }); // Registering MediatR
-
 builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);  // Registering Validator - FluentValidation
 
+// Registering Data Services
 builder.Services.AddMarten(opt =>
 {
     opt.Connection(builder.Configuration.GetConnectionString("Database")!);
     opt.Schema.For<ShoppingCart>().Identity(x => x.Username);
 }).UseLightweightSessions(); // Registering Marten ORM
 
-// Registering Repositories
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
 builder.Services.Decorate<IBasketRepository, CachedBasketRepository>(); // Registering decorator CachedBasketRepository - Scrutor
+
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetConnectionString("Redis");
+}); // Registering Redis cache.
 
 // Registering gRPC Services
 builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(options =>
@@ -48,16 +55,13 @@ builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(
     options.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]!);
 });
 
-builder.Services.AddStackExchangeRedisCache(options =>
-{
-    options.Configuration = builder.Configuration.GetConnectionString("Redis");
-}); // Registering Redis cache.
+// Registering Async Communication Services
+builder.Services.AddMessageBroker(builder.Configuration);
 
+// Registering Cross-Cutting Concerns
 builder.Services.AddExceptionHandler<CustomExceptionHandler>(); // Registering Global Exception Handler
-
-// Register HealthChecks
 builder.Services.AddHealthChecks()
-                .AddNpgSql(builder.Configuration.GetConnectionString("Database")!);
+                .AddNpgSql(builder.Configuration.GetConnectionString("Database")!); // Register HealthChecks
 
 var app = builder.Build();
 
