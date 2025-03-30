cf9f6c42adda71a3cca82dcd53ad1739b7c15c38
hungnqph19112 <nqh1703k2@gmail.com>
2025-03-30 14:16:06 +0700

    Building Block: update PagingParam Model

--- src/BuidingBlocks/BuidingBlocks/Pagination/PaginatedResult.cs
+++ src/BuidingBlocks/BuidingBlocks/Pagination/PaginatedResult.cs
@@ -1,10 +1,10 @@
 ﻿namespace BuidingBlocks.Pagination
 {
-    public class PaginatedResult<TEntity>(int pageIndex, int pageSize, long count, IEnumerable<TEntity> data) where TEntity : class
+    public class PaginatedResult<TEntity>(int pageNumber, int pageSize, int totalPage, IEnumerable<TEntity> data) where TEntity : class
     {
-        public int PageIndex { get; } = pageIndex;
+        public int PageNumber { get; } = pageNumber;
         public int PageSize { get; } = pageSize;
-        public long Count { get; } = count;
+        public int TotalPage { get; } = totalPage;
         public IEnumerable<TEntity> Data { get; } = data;
     }
 }
--- src/BuidingBlocks/BuidingBlocks/Pagination/PaginationRequest.cs
+++ src/BuidingBlocks/BuidingBlocks/Pagination/PaginationRequest.cs
@@ -1,4 +1,4 @@
 ﻿namespace BuidingBlocks.Pagination
 {
-    public record PaginationRequest(int PageIndex = 0, int PageSize = 10);
+    public record PaginationRequest(int PageNumber = 1, int PageSize = 10);
 }
